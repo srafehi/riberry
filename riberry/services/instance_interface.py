1ac56c860d43d974cfefa004ab3e18b72b044659
Shady Rafehi <shadyrafehi@gmail.com>
2018-05-22 14:58:20 +1000

    Add group loading to instance interface

--- riberry/services/instance_interface.py
+++ riberry/services/instance_interface.py
@@ -14,12 +14,17 @@ def instance_interface_by_id(instance_interface_id) -> model.interface.Applicati
     return model.interface.ApplicationInstanceInterface.query().filter_by(id=instance_interface_id).one()
 
 
-def create_instance_interface(instance_id, interface_id) -> model.interface.ApplicationInstanceInterface:
+def create_instance_interface(instance_id, interface_id, group_names) -> model.interface.ApplicationInstanceInterface:
     instance = services.application_instance.application_instance_by_id(application_instance_id=instance_id)
     interface = services.application_interface.application_interface_by_id(application_interface_id=interface_id)
 
     assert instance.application.id == interface.application.id, \
         'Instance and interface do not belong to the same application'
+
+    groups = []
+    for group_name in group_names:
+        group = model.group.Group.query().filter_by(name=group_name).one()
+        groups.append(group)
 
     instance_interface = model.interface.ApplicationInstanceInterface(
         instance=instance,
@@ -27,6 +32,16 @@ def create_instance_interface(instance_id, interface_id) -> model.interface.Appl
     )
 
     policy.context.authorize(instance_interface, action='create')
+
+    for group in groups:
+        group_association = model.group.ResourceGroupAssociation(
+            resource_id=group.id,
+            resource_type=model.group.ResourceType.application_instance_interface,
+            group=group
+        )
+        model.conn.add(group_association)
+
+
     model.conn.add(instance_interface)
     model.conn.commit()
     return instance_interface
