9d5dbb7cb22164df6f1cefe13833115ab1bcac7b
Shady Rafehi <shadyrafehi@gmail.com>
2018-05-27 00:25:17 +1000

    Returning envelopes for auth resources

--- riberry/rest/views/auth/__init__.py
+++ riberry/rest/views/auth/__init__.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restplus import Namespace, Resource
 
-from riberry.rest import services
+from riberry.rest import services, views
 from . import args
 
 api = Namespace('auth', description='Authentication and authorization')
@@ -13,7 +13,8 @@ class AccessTokenResource(Resource):
     def post(self):
         data = request.json
         username, password = data.get('username'), data.get('password')
-        return services.auth.authenticate_user(username=username, password=password)
+        response = services.auth.authenticate_user(username=username, password=password)
+        return views.make_response(response)
 
 
 @api.route('/groups', endpoint='auth-group:collection')
@@ -24,47 +25,53 @@ class GroupCollectionResource(Resource):
 
     def post(self):
         data = request.json
-        return services.auth.create_group(
-            name=data['name']
-        )
+        response = services.auth.create_group(name=data['name'])
+        return views.make_response(response)
 
 
 @api.route('/groups/<id>', endpoint='auth-group:resource')
 class GroupInstanceResource(Resource):
 
     def get(self, id):
-        return services.auth.group_by_id(group_id=id)
+        response = services.auth.group_by_id(group_id=id)
+        return views.make_response(response)
 
 
 @api.route('/groups/<id>/users', endpoint='auth-group:user:collection')
 class GroupInstanceUserCollectionResource(Resource):
 
     def get(self, id):
-        return services.auth.users_for_group_id(group_id=id)
+        response = services.auth.users_for_group_id(group_id=id)
+        return views.make_response(response)
 
 
 @api.route('/groups/<id>/forms', endpoint='auth-group:form:collection')
 class GroupFormCollectionResource(Resource):
 
     def get(self, id):
-        return services.auth.forms_for_group_id(group_id=id)
+        response = services.auth.forms_for_group_id(group_id=id)
+        return views.make_response(response)
 
 
 @api.route('/groups/<id>/users/<user_id>', endpoint='auth-group:user:resource')
 class GroupUserCollectionResource(Resource):
 
     def post(self, id, user_id):
-        return services.auth.add_user_to_group(group_id=id, user_id=user_id)
+        response = services.auth.add_user_to_group(group_id=id, user_id=user_id)
+        return views.make_response(response)
 
     def delete(self, id, user_id):
-        return services.auth.remove_user_from_group(group_id=id, user_id=user_id)
+        response = services.auth.remove_user_from_group(group_id=id, user_id=user_id)
+        return views.make_response(response)
 
 
 @api.route('/groups/<id>/forms/<form_id>', endpoint='auth-group:form:resource')
 class GroupFormResource(Resource):
 
     def post(self, id, form_id):
-        return services.auth.add_form_to_group(group_id=id, form_id=form_id)
+        response = services.auth.add_form_to_group(group_id=id, form_id=form_id)
+        return views.make_response(response)
 
     def delete(self, id, form_id):
-        return services.auth.remove_form_from_group(group_id=id, form_id=form_id)
+        response = services.auth.remove_form_from_group(group_id=id, form_id=form_id)
+        return views.make_response(response)
