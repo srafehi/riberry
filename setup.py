dbf8a9438ba792941836827a38b9a6f413e96c79
Shady Rafehi <shadyrafehi@nbnco.com.au>
2018-06-08 14:44:43 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riberry',
+    version='0.0.1',
+    author='Shady Rafehi',
+    url='https://github.com/srafehi/riberry',
+    author_email='shadyrafehi@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'redis',
+        'croniter',
+        'pendulum',
+        'sqlalchemy',
+        'celery==4.2.0rc4',
+        'pyyaml',
+        'pyjwt',
+        'toml'
+    ],
+)
