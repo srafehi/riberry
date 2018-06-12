9e8096653b037c114cc759b723355061272cce9f
Unknown <shadyrafehi@gmail.com>
2018-06-12 20:45:51 +1000

    Added service to extract job artifact

--- riberry/services/job_executions.py
+++ riberry/services/job_executions.py
@@ -4,3 +4,8 @@ from riberry import model, policy
 @policy.context.post_authorize(action='view')
 def job_execution_by_id(execution_id):
     return model.job.JobExecution.query().filter_by(id=execution_id).one()
+
+
+@policy.context.post_authorize(action='view')
+def job_artifact_by_id(execution_id):
+    return model.job.JobExecutionArtifact.query().filter_by(id=execution_id).one()
