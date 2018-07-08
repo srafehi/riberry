5c3f7cc67760534c9b78304650cdd97b2b2248d3
Unknown <shadyrafehi@gmail.com>
2018-07-08 19:44:09 +1000

    Renaming parameter

--- riberry/services/job_executions.py
+++ riberry/services/job_executions.py
@@ -7,5 +7,5 @@ def job_execution_by_id(execution_id):
 
 
 @policy.context.post_authorize(action='view')
-def job_artifact_by_id(execution_id):
-    return model.job.JobExecutionArtifact.query().filter_by(id=execution_id).one()
+def job_artifact_by_id(artifact_id):
+    return model.job.JobExecutionArtifact.query().filter_by(id=artifact_id).one()
