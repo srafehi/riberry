d79cc32e7afbf69f76c223a6323b0083d05c5d3b
Unknown <shadyrafehi@gmail.com>
2019-04-14 23:32:47 +1000

    import RiberryApplicationConfig

--- riberry/app/__init__.py
+++ riberry/app/__init__.py
@@ -1,3 +1,3 @@
 from .env import current_context, current_riberry_app
-from .base import RiberryApplication
+from .base import RiberryApplication, RiberryApplicationConfig
 from . import actions, context, tasks, util, addons, backends
