4a0c84f2b283d24e07977cde19078af4a80ad26a
Unknown <shadyrafehi@gmail.com>
2019-09-09 23:07:22 +1000

    Bump version (post-release fix)

--- riberry/__init__.py
+++ riberry/__init__.py
@@ -1,6 +1,6 @@
 from riberry import config, plugins, model, celery, policy, services, exc, app
 
-__version__ = '0.10.8'
+__version__ = '0.10.9'
 
 config.config.enable()
 config.config.authentication.enable()
