5e64c8f7021cec3ac7522b6dbf28adc313a4cbf4
Unknown <shadyrafehi@gmail.com>
2019-09-29 22:02:35 +1000

    Updated cli for pool backend

--- riberry/cli/commands/run.py
+++ riberry/cli/commands/run.py
@@ -1,4 +1,11 @@
+import importlib
+import os
+
 import click
+
+import riberry
+from riberry.app.backends.impl.pool.base import RiberryPoolBackend
+from riberry.app.backends.impl.pool.log import configure as log_configure
 from ..root import cli
 
 
@@ -8,10 +15,19 @@ def run():
 
 
 @run.command()
-@click.pass_context
-def pool(ctx: click.Context):
-    print('not implemented')
-    ctx.exit(1)
+@click.option('--module', '-m', required=True, help='Module containing Riberry pool application')
+@click.option('--instance', '-i', help='Riberry application instance to run')
+@click.option('--log-level', '-l', default='ERROR', help='Log level')
+@click.option('--concurrency', '-c', default=None, help='Task concurrency', type=int)
+def pool(module, instance, log_level, concurrency):
+    if instance is not None:
+        os.environ['RIBERRY_INSTANCE'] = instance
+
+    log_configure(log_level=log_level)
+    importlib.import_module(module, instance)
+    backend: RiberryPoolBackend = riberry.app.current_riberry_app.backend
+    backend.task_queue.limit = concurrency
+    backend.start()
 
 
 cli.add_command(run)
