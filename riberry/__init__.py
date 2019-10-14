5738ce9db719156ed347162f5379cf3439cc9611
Unknown <shadyrafehi@gmail.com>
2019-10-15 09:31:21 +1100

    Bump version

--- riberry/__init__.py
+++ riberry/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from riberry import log, config, plugins, model, celery, policy, services, exc, app
 
-__version__ = '0.10.15'
+__version__ = '0.10.16'
 
 log.root_name = __name__
 log.logger = logging.getLogger(log.root_name)
