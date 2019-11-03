1b360649b3738685c9bf6e321874e764cc1655cb
Unknown <shadyrafehi@gmail.com>
2019-11-03 14:03:30 +1100

    Dispose sqla engine when process is a fork + better type hinting

--- riberry/model/__init__.py
+++ riberry/model/__init__.py
@@ -1,33 +1,64 @@
-from typing import Union
-
+import os
+from typing import Union, Any
 import sqlalchemy
 import sqlalchemy.orm
 import sqlalchemy.pool
+import sqlalchemy.engine
 
 from . import misc, application, group, auth, interface, job, base
 from ..util.misc import import_from_string
+from .. import log
+
+log = log.make(__name__)
 
 ScopedSessionExt = Union[sqlalchemy.orm.Session, sqlalchemy.orm.scoping.ScopedSession]
 
 
 class __ModelProxy:
     raw_session: ScopedSessionExt = None
-    raw_engine = None
+    raw_engine: sqlalchemy.engine.Engine = None
+    _pid_created_in: int = os.getpid()
 
-    def __getattr__(self, item):
+    def _check_pid(self):
+        """ Disposes SQLAlchemy engine if current process is a fork. """
+
+        current_pid = os.getpid()
+        if self._pid_created_in != current_pid:
+            log.debug(f'riberry.model.conn:: Process forked from pid {self._pid_created_in}, disposing of sqla engine')
+            self._pid_created_in = current_pid
+            self.dispose_engine()
+
+    def __getattr__(self, item: str) -> Any:
+        self._check_pid()
         return getattr(self.raw_session, item)
 
-    def __enter__(self):
+    def __enter__(self) -> '__ModelProxy':
+        self._check_pid()
+        self.raw_session()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type is not None:
             self.raw_session.rollback()
-        self.raw_session.remove()
+        self.dispose_session()
+
+    def dispose_engine(self):
+        """ Disposes of the current SQLAlchemy engine. """
+        self.dispose_session()
+        try:
+            self.raw_engine.dispose()
+        except:
+            log.exception('riberry.model.conn:: Encountered an error while disposing current sqla engine')
+
+    def dispose_session(self):
+        """ Disposes of the current SQLAlchemy session. """
+        try:
+            self.raw_session.remove()
+        except:
+            log.exception('riberry.model.conn:: Encountered an error while removing current sqla session')
 
 
-# noinspection PyTypeChecker
-conn: ScopedSessionExt = __ModelProxy()
+conn: Union[ScopedSessionExt, __ModelProxy] = __ModelProxy()
 
 
 def init(url='sqlite://', engine_settings=None, connection_arguments=None):
