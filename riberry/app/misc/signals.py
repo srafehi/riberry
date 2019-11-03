b38862fa772125c9dd982ae766754e53d476a195
Unknown <shadyrafehi@gmail.com>
2019-11-03 14:29:38 +1100

    Patch celery.concurrency.asynpool.AsynPool.on_process_up
    The patch is used to dispose of the active sqla engine in the main celery process whenever a new worker process is spawned.
    
    This is used to avoid shared db sessions after a fork.

--- riberry/app/misc/signals.py
+++ riberry/app/misc/signals.py
@@ -4,6 +4,7 @@ import riberry
 from ..util.events import create_event
 
 
+log = riberry.log.make(__name__)
 __stream_cache = {}
 
 
@@ -18,16 +19,49 @@ def try_cache_stream(root_id, stream_name, stream_state):
     return False
 
 
+@signals.celeryd_after_setup.connect
+def celeryd_after_setup(**_):
+    """
+    Dispose of Riberry's SQLAlchemy engine after the Celery daemon has completed setup.
+    """
+    riberry.model.conn.dispose_engine()
+
+
 @signals.worker_process_init.connect
 def worker_process_init(*args, **kwargs):
-    riberry.model.conn.raw_engine.dispose()
-    riberry.model.conn.remove()
+    """
+    Dispose of Riberry's SQLAlchemy engine in newly initialized worker processes.
+
+    This is used to prevent database sessions being mistakenly used by both
+    the parent and child process.
+    """
+
+    riberry.model.conn.dispose_engine()
 
 
-@signals.celeryd_after_setup.connect
-def celeryd_after_setup(*args, **kwargs):
-    riberry.model.conn.raw_engine.dispose()
-    riberry.model.conn.remove()
+@signals.worker_ready.connect
+def worker_ready(sender, **_):
+    """
+    Patch "celery.concurrency.asynpool.AsynPool.on_process_up" to dispose of
+    Riberry's SQLAlchemy engine whenever a new worker process is created via
+    prefork.
+
+    This is used to prevent database sessions being mistakenly used by both
+    the parent and child process.
+    """
+
+    pool = getattr(sender.pool, '_pool', None)
+    if pool is None:
+        return
+
+    on_process_up_original = pool.on_process_up
+
+    def on_process_up(proc):
+        riberry.model.conn.dispose_engine()
+        on_process_up_original(proc)
+
+    pool.on_process_up = on_process_up
+    riberry.model.conn.dispose_engine()
 
 
 @signals.before_task_publish.connect
