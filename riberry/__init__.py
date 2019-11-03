49a75464587aceecd7e44dc5be25cb5015085e4d
Unknown <shadyrafehi@gmail.com>
2019-11-03 13:33:35 +1100

    Ensure root logger is configured prior to importing any other modules

--- riberry/__init__.py
+++ riberry/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from riberry import log, config, plugins, model, celery, policy, services, exc, app
+from riberry import log
 
 __version__ = '0.10.16'
 
@@ -8,6 +8,7 @@ log.root_name = __name__
 log.logger = logging.getLogger(log.root_name)
 log.init()
 
+from riberry import config, plugins, model, celery, policy, services, exc, app
 
 config.config.enable()
 config.config.authentication.enable()
